750e61f44439a36d9582daf149d0dc1b2b9a72c7
Sébastien CHAZALLET <s.chazallet@gmail.com>
2012-11-06 17:02:18 +0100

    Utilisation de Custom pour GalerieC

--- module/Galerie/src/Galerie/Model/GalerieC.php
+++ module/Galerie/src/Galerie/Model/GalerieC.php
@@ -2,7 +2,9 @@
 
 namespace Galerie\Model;
 
-class GalerieC
+use Custom\Model\Entity;
+
+class GalerieC extends Entity
 {
     public $id;
     public $id_user;
@@ -32,40 +34,5 @@ class GalerieC
     protected $primary_columns = array(
         'id',
     );
-
-    public function exchangeArray($data, $overwrite=true)
-    {
-        foreach($this->columns as $col) {
-            if (array_key_exists($col, $data)) {
-                $this->$col = $data[$col];
-            } elseif ($overwrite) {
-                $this->$col = null;
-            }
-        }
-    }
-
-    public function toArray() {
-        $result = array();
-        foreach($this->columns as $col) {
-            $result[$col] = $this->$col;
-        }
-        return $result;
-    }
-
-    public function toUpdatableArray() {
-        $result = array();
-        foreach($this->updatable_columns as $col) {
-            $result[$col] = $this->$col;
-        }
-        return $result;
-    }
-
-    public function toPrimaryArray() {
-        $result = array();
-        foreach($this->primary_columns as $col) {
-            $result[$col] = $this->$col;
-        }
-        return $result;
-    }
 
 }
