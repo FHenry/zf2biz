be46560bc8bd090930c4d0ede944a9b74ab59a15
Sébastien CHAZALLET <s.chazallet@gmail.com>
2012-11-07 18:06:51 +0100

    Requête et Réponse: outils disponibles

--- module/Galerie/src/Galerie/Controller/IndexController.php
+++ module/Galerie/src/Galerie/Controller/IndexController.php
@@ -40,12 +40,34 @@ class IndexController extends AbstractActionController
 
     public function editAction() 
     { 
-        return array(); 
+        // Récupération de l'objet de travail
+        $id = $this->params()->fromRoute('id', null);
+        $galerie = $this->_getGalerieTable()->any($id);
+
+        // Récupération de l'objet requête
+        $request = $this->getRequest();
+        if ($request->isPost()) {
+            // Validation des données
+
+            if (true) {//TODO: Si les données sont valides
+                // Sauvegarde des données
+                // $this->_getGalerieTable()->save(?TODO?);
+
+                // Redirection 
+                return $this->redirect()->toRoute('galerie');
+            }
+        }
+        return new ViewModel(array(
+            'id' => $id,
+            'galerie' => $galerie,
+        ));
     } 
 
     public function delAction() 
     { 
-        return array(); 
+        return $this->redirect()->toRoute('galerie/view', array(
+            'id' => $id,
+        ));
     } 
 
     public function viewAction() 
