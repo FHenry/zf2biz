972279db0edd2e87bbb4dde71ed5a091c76e5a3e
Sébastien CHAZALLET <s.chazallet@gmail.com>
2012-11-15 12:49:15 +0100

    Formulaire : étape 1, rajout de l'objet formulaire

---
+++ module/Galerie/src/Galerie/Form/GalerieForm.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Galerie\Form;
+
+use Zend\Form\Form;
+
+class GalerieForm extends Form
+{
+
+    public function __construct($name=null)
+    {
+        parent::__construct('galerie');
+        $this->setAttribute('method', 'post');
+        $this->add(array(
+            'name' => 'id',
+            'attributes' => array(
+                'type' => 'hidden',
+            ),
+        ));
+        $this->add(array(
+            'name' => 'name',
+            'attributes' => array(
+                'type' => 'text',
+            ),
+            'options' => array(
+                'label' => 'Name',
+            )
+        ));
+        $this->add(array(
+            'name' => 'description',
+            'attributes' => array(
+                'type' => 'text',
+            ),
+            'options' => array(
+                'label' => 'Description',
+            )
+        ));
+        $this->add(array(
+            'name' => 'submit',
+            'attributes' => array(
+                'type' => 'submit',
+                'value' => 'Valider',
+                'id' => 'submit_galerie_form',
+            ),
+            'options' => array(
+                'label' => 'Description',
+            )
+        ));
+    }
+
+}
