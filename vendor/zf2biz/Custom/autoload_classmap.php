e36bdfb4ca53888eff39a4e89b328f056a0f6b4a
Sébastien CHAZALLET <s.chazallet@gmail.com>
2012-11-18 16:25:17 +0100

    Rajout d'une aide d'action MessageGetter

--- vendor/zf2biz/Custom/autoload_classmap.php
+++ vendor/zf2biz/Custom/autoload_classmap.php
@@ -3,4 +3,5 @@
 return array(
     'Custom\Model\Entity' => __DIR__ . '/Model/Entity.php',
     'Custom\Model\Manager' => __DIR__ . '/Model/Manager.php',
+    'Custom\Mvc\Controller\Plugin\MessageGetter' => __DIR__ . '/Mvc/Controller/Plugin/MessageGetter.php',
 );
