17f50f55e53c8dedc74f1b3f1d657026821e5370
zendformation <zendformation@localhost.localdomain>
2014-01-09 14:36:30 +0100

    f

--- module/Galerie/autoload_classmap.php
+++ module/Galerie/autoload_classmap.php
@@ -3,4 +3,8 @@
 return array(
     'Galerie\Controller\IndexController' =>
         __DIR__ . '/src/Galerie/Controller/IndexController.php',
+	'Custom\Model\Entity'=>
+		__DIR__ . '/../../vendor/zf2biz/Custom/Model/Entity.php',
+	'Custom\Model\Manager'=>
+		__DIR__ . '/../../vendor/zf2biz/Custom/Model/Manager.php',
 );
