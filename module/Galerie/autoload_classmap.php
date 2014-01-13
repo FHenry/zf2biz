34d986bdc93559f76a4b8df81be9668ce8134a17
zendformation <zendformation@localhost.localdomain>
2014-01-13 17:36:20 +0100

    sdsdf

--- module/Galerie/autoload_classmap.php
+++ module/Galerie/autoload_classmap.php
@@ -12,4 +12,6 @@ return array(
 		__DIR__ . '/../../vendor/zf2biz/Custom/Form/AbstractForm.php',
 	'Custom\Mvc\Controller\Plugin\MessageGetter' =>
 		__DIR__ . '/../../vendor/zf2biz/Custom/Mvc/Controller/Plugin/MessageGetter.php',
+		'Custom\View\Helper\Format' =>
+		__DIR__ . '/../../vendor/zf2biz/Custom/View/Helper/Format.php',
 );
